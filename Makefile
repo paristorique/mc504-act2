198b7a3a67a04caa7c3336f1766efd4ad269b71c
Stephane Ariel Jerome Ammar <ra149071@xaveco.lab.ic.unicamp.br>
2014-04-15 19:04:37 -0300

    Message de validation

---
+++ Makefile
@@ -0,0 +1,7 @@
+all: part1
+
+part1:
+	gcc -g -pthread drawsavage.c savage.c savage.h -o savage
+
+
+
---
+++ savage.h
@@ -0,0 +1,28 @@
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define SLEEPING 0
+#define HUNGRY 1
+#define EATING 2
+#define WALKING 3
+#define WANDER 4
+
+struct Pot {
+    int meals;
+    int capacity;
+    int x;
+    int y;
+};
+
+struct Savage {
+    int status;
+    int x;
+    int y;
+	int position;
+};
+
+struct Pot * pot ();
+struct Savage * cooker ();
+struct Savage * newSavage (int,int,int);
+void start (int,int,int,int,int);
